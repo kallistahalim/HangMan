b045bde73e46024d61c1a825839f02b4e71f324c
kallistahalim <kallistahalim@gmail.com>
2020-04-21 00:10:26 -0700

    omg i cant believe i did it without watching video tutorial

--- asset/app.js
+++ asset/app.js
@@ -4,18 +4,22 @@ var numLose;
 var wordToGuess = ["Coco", "Puma", "Treats", "Bandana", "Cuddles", "Pets"];
 var numberOfGuess = 9;
 var yourGuess = [];
+var wordDisplay = [];
+var computerChoice = wordToGuess[Math.floor(Math.random() * wordToGuess.length)].toUpperCase();
+var wordToLetters = computerChoice.split("");
 
 
 //START THE GAME
 
 function startGame() {
     //set up initial state before game is played
-    var computerChoice = wordToGuess[Math.floor(Math.random() * wordToGuess.length)].toUpperCase();
-    var wordToLetters = computerChoice.split("");
-    var wordDisplay = [];
+    computerChoice = wordToGuess[Math.floor(Math.random() * wordToGuess.length)].toUpperCase();
+    wordToLetters = computerChoice.split("");
+    wordDisplay = [];
     numberOfGuess = 9;
     numWin = 0;
     numLose = 0;
+    yourGuess = [];
 
     //create _ _ _ _ based on how many letters in the word
     for(var i = 0; i < wordToLetters.length; i ++) {
@@ -36,6 +40,28 @@ function startGame() {
 
 startGame();
 
-document.onkeypress = function() {
+document.onkeypress = function(event) {
+    var letter = event.key.toUpperCase();
+    var isLetterPrinted = false;
+
+    for(var i = 0; i < wordToLetters.length; i++) {
+        
+        if(letter === wordToLetters[i]) {
+            isLetterPrinted = true;
+
+            //test
+            console.log(isLetterPrinted);
+            if(isLetterPrinted === true) {
+                wordDisplay[i] = letter;
+                document.querySelector("#wordToGuess").innerHTML = wordDisplay.join(" ");
+            }
+            }
+        }
+        
     
+    
+    //tests
+    console.log(letter);
+    
+
 }
